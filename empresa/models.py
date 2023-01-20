8e2a359a851c8bc2384d2116d0d2d6269bb3c674
Pedro Feitosa <pedr.fts@gmail.com>
2023-01-20 12:18:06 -0300

    Profissional

--- empresa/models.py
+++ empresa/models.py
@@ -49,10 +49,30 @@ class Vagas(models.Model):
     titulo = models.CharField(max_length=30)
     nivel_experiencia = models.CharField(max_length=2, choices=choices_experiencia)
     data_final = models.DateField()
+    email = models.EmailField()
     status = models.CharField(max_length=30, choices=choices_status)
     tecnologias_dominadas = models.ManyToManyField(Tecnologias)
     tecnologias_estudar = models.ManyToManyField(Tecnologias, related_name='estudar')
 
+    # Tem como fazer a BARRA DE PROGRESSÃO 2 formas
+    def progresso(self):
+        
+        # 1 forma
+        # if self.status == "I":
+        #     return 20
+        # elif self.status == "C":
+        #     return 40
+        # elif self.status == "E":
+        #     return 60
+        # elif self.status == "D":
+        #     return 80
+        # elif self.status == "F":
+        #     return 100
+        
+        # 2 Forma
+        x = [((i+1)*20,j[0]) for i, j in enumerate(self.choices_status)]
+        x = list(filter(lambda x: x[1] == self.status, x))[0][0]
+        return x
 
     def __str__(self):
         return self.titulo
